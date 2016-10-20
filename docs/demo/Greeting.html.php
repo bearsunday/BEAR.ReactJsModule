18522bb3f1207adf8e5f2cb1c18ed76e415c7024
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-10-20 21:31:23 +0900

    add demo

---
+++ docs/demo/Greeting.html.php
@@ -0,0 +1,20 @@
+<?php
+
+use BEAR\ReactJsModule\Ssr;
+/* @var $ssr Ssr */
+list($markup, $script) = $ssr->render(['hello']);
+
+return <<<"EOT"
+<!doctype>
+<html>
+<head>
+    <title>{$ssr->escape('title')}</title>
+</head>
+<body>
+<div id="root">{$markup}</div>
+<script src="build/react.bundle.js"></script>
+<script src="build/app.bundle.js"></script>
+<script>{$script}</script>
+</body>
+</html>
+EOT;
