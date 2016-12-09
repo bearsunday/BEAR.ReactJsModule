1ad2f731da09f1c6d937fa2edce3dd7d9655241a
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-12-09 18:30:59 +0900

    remove global.Provider

--- docs/demo/ui/ssr/app.js
+++ docs/demo/ui/ssr/app.js
@@ -1,7 +1,5 @@
 import configureStore from '../common/store/configureStore';
 import App from '../common/components/App';
-import { Provider } from 'react-redux';
 
 global.App = App;
-global.Provider = Provider;
 global.configureStore = configureStore;
