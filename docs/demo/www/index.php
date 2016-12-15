71e8223e536d17c6a3904645c81bbf09bc6c9a1a
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-12-15 12:50:53 +0900

    remove use statement

--- docs/demo/www/index.php
+++ docs/demo/www/index.php
@@ -4,11 +4,10 @@ require dirname(__DIR__, 3) . '/vendor/autoload.php';
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
 
 use BEAR\ReactJsModule\ReduxModule;
-use BEAR\Resource\ResourceObject;
 use Ray\Di\Injector;
 
 $injector = new Injector(new ReduxModule(__DIR__ . '/build', 'ssr_app'));
 $index = $injector->getInstance(Greeting::class);
-/* @var $index ResourceObject */
+/* @var $index Greeting */
 
 echo $index;
