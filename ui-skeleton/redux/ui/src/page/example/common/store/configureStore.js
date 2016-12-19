2edc77940e19182455671da0d76e0bdbb8474965
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-12-19 16:29:34 +0900

    add hmr and redux logger to configureState

--- ui-skeleton/redux/ui/src/page/example/common/store/configureStore.js
+++ ui-skeleton/redux/ui/src/page/example/common/store/configureStore.js
@@ -1,10 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
 export default function configureStore(preloadedState) {
   const store = createStore(
-    rootReducer,
-    preloadedState
+      rootReducer,
+      preloadedState,
+      compose(
+          applyMiddleware(thunkMiddleware, createLogger()),
+          window.devToolsExtension ? window.devToolsExtension() : f => f
+      )
   );
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
+      store.replaceReducer(nextReducer);
+    });
+  }
   return store;
 }
