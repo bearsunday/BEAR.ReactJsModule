f04152815d9361ca74d32239e5fd601487419f52
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-01-23 11:50:02 +0900

    use src directory

---
+++ docs/demo/ui/src/app/client.js
@@ -0,0 +1,5 @@
+import configureStore from '../../src/store/configureStore';
+import App from '../../src/components/App';
+
+global.App = App;
+global.configureStore = configureStore;
--- docs/demo/ui/webpack.config.js
+++ docs/demo/ui/webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: {
     react: './ui/react-bundle',
-    ssr_app: './ui/app/server',
-    app: './ui/app/client',
+    ssr_app: './ui/src/app/server',
+    app: './ui/src/app/client',
   },
   output: {
     path: path.join(__dirname, '/../www/build'),
