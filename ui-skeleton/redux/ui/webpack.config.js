3ed6abcd6c65ae8ebc6e65a77910e86417acaebc
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-01-26 10:47:49 +0900

    install react hot loader v3

--- ui-skeleton/redux/ui/webpack.config.js
+++ ui-skeleton/redux/ui/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['react-hot-loader', 'babel-loader'],
+        loaders: ['babel-loader'],
         exclude: /(node_modules)/,
       },
       {
